541ff484ea0ec6a3a9d77abe1a61ead88fd25b03
Aaron Beavers <foamy125@gmail.com>
2018-03-03 16:44:07 -0600

    refactored

--- index.js
+++ index.js
@@ -7,19 +7,23 @@ function minutes(n) {
 function seconds (n) {
     return n * 1000;
 }
-function isSaiWindow(w) {
-    console.log("active window",w)
+function saveSai(w) {
+    console.log("active window",w);
     if(/sai/i.test(w.owner.name)) {
         console.log('saving file');
         robotjs.keyTap("s","control");
     }
 }
-var timeout (ms)=> new Promise ((s,f) => setTimeout(s,ms))
+var timeout = (ms) => new Promise ((s,f) => setTimeout(s,ms)),
+    bind = (f,...b) => (...args) => f(...b,...args);
 
-setInterval(() => {
-    activeWin()
-        .then(isSaiWindow)
-},minutes(2));
+
+var autosave = (i) => activeWin()
+    .then(saveSai)
+    .then(bind(timeout,minutes(2)))
+    .then(bind(autosave,i));
+
+autosave(minutes(2))
 
 
 
