d10e95a89a652f7f667bd0319a6f68cd7720650b
Aaron Beavers <foamy125@gmail.com>
2018-03-03 16:38:45 -0600

    extended timer

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ function seconds (n) {
 function isSaiWindow(w) {
     console.log("active window",w)
     if(/sai/i.test(w.owner.name)) {
+        console.log('saving file');
         robotjs.keyTap("s","control");
     }
 }
+var timeout (ms)=> new Promise ((s,f) => setTimeout(s,ms))
+
 setInterval(() => {
     activeWin()
         .then(isSaiWindow)
-},1000);
+},minutes(2));
 
 
 
